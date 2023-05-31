effa8c515040ad70b6ec4bb659776271c685e480
Simon Xie <simon.xie@shopcanal.com>
2023-05-31 14:54:22 -0700

    z

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -30,6 +30,6 @@ STRIPE_PUBLIC_KEY = "pk_test_51MyNecBPRjE4EZIhfqc07HDwRVKCabyigjsSjHfCTpoaanifae
 STRIPE_SECRET_KEY = "sk_test_51MyNecBPRjE4EZIhe1Eujz6Vt8TUlIa8BFRqUqqXBPl81vLIsnS7ZG4B06I2B5q881nTrS2rtyIWlWcxFbHDB1Gw00VkzxUgWJ"
 
 
-SHOPCANAL_API_BASE_URL = "https://api.shopcanal.com/platform"
+SHOPCANAL_API_BASE_URL = "https://api-develop.shopcanal.com/platform"
 CANAL_APP_ID = "c13012fd-fd99-458b-a89e-4bf1e4cbae03"
 CANAL_ACCESS_TOKEN = "48c0e282456c4d029978259ab765e1e8"
